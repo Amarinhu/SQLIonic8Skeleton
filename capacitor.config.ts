c557f1348f33d6e197f0c37b192b02e928b58360
Amaral <amaral.passosgabriel@gmail.com>
2025-03-07 11:16:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IonicSkeleton',
+  webDir: 'dist'
+};
+
+export default config;
